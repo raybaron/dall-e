87e31c25b3c2fb2bcc114c27ba47b40a0fcc1275
Rayyan Pistawala <rayyan.pistawala@fulcroworld.com>
2023-03-26 17:27:46 +0530

    first commit

---
+++ server/mongodb/connect.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = (url)=>{
+    mongoose.set('strictQuery',true);
+
+    mongoose.connect(url)
+    .then(()=> console.log('mongo db connected'))
+    .catch((error)=>console.log('error name:',error))
+}
+
+export default connectDB;
